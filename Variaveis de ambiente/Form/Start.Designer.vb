f0ab31cd41cfb29e600181d6dc88bce788972a8a
Emmanoel Francisco <emmanoel.ti@hotmail.com>
2021-06-17 10:58:57 -0300

    Adicionar arquivos de projeto.

---
+++ Variaveis de ambiente/Form/Start.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Start
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbVar = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lbVar
+        '
+        Me.lbVar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lbVar.FormattingEnabled = True
+        Me.lbVar.Location = New System.Drawing.Point(0, 0)
+        Me.lbVar.Name = "lbVar"
+        Me.lbVar.Size = New System.Drawing.Size(725, 284)
+        Me.lbVar.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 284)
+        Me.Controls.Add(Me.lbVar)
+        Me.Name = "Form1"
+        Me.Text = "Variavies de ambiente"
+        Me.ResumeLayout(false)
+
+End Sub
+    Friend WithEvents lbVar As System.Windows.Forms.ListBox
+
+End Class
---
+++ Variaveis de ambiente/Form/Start.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Environment
+Public Class Start
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbVar.Items.Clear()
+        lbVar.Items.Add("Meus Documentos: " & GetFolderPath(SpecialFolder.MyDocuments))
+        lbVar.Items.Add("Arquivos de Programas (x86): " & GetFolderPath(SpecialFolder.ProgramFiles))
+        lbVar.Items.Add("Pasta System32: " & GetFolderPath(SpecialFolder.System))
+        lbVar.Items.Add(GetFolderPath(SpecialFolder.Desktop))
+        lbVar.Items.Add("----------------------------")
+        lbVar.Items.Add("Drives do Sistema: " & Join(GetLogicalDrives, ", "))
+        lbVar.Items.Add("Nome do Computador: " & MachineName)
+        lbVar.Items.Add("Versão do Sistema: " & OSVersion.VersionString)
+        lbVar.Items.Add("Diretorio Atual: " & CurrentDirectory)
+        lbVar.Items.Add("Contador de Processador: " & ProcessorCount)
+        lbVar.Items.Add("Diretorio EXE:" & Environment.CommandLine)
+        lbVar.Items.Add("Se for True é 64Bits: " & Environment.Is64BitOperatingSystem)
+    End Sub
+End Class
